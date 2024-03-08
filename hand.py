2faa622bceec7ca3b203697c2afc7cbbff54a285
Gaurish Garg <gaurish_garg@outlook.com>
2024-03-08 06:52:33 +0000

    dummy123

--- hand.py
+++ hand.py
@@ -36,6 +36,16 @@ async def websocket_server():
         print(mydict)
         st.write(mydict)
         url = 'https://streamcom.onrender.com/getdata'
+        data = {"dummy": "dummy"}
+        response = requests.post(url, json=data)
+        if response.status_code == 200:
+            st.write('POST request successful!')
+        else:
+            st.write('POST request failed:', response.status_code)
+    
+        # Send POST request
+
+
         if mydict["browserId"]!=None:
 
         # Data to be sent in the POST request
